55cfb9e8c520311bf80d070e73f4f42d38833fb3
FilipeRicardoSilva <filipe.silva@sptech.school>
2023-10-08 20:16:19 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import api from "./woof-joy-api";
+import { useState } from "react";
 import "./css/index.css";
 import "./css/responsivo.css";
-
+import Index from "./components/indexComponent";
+import CardParceiro from "./components/feed/card-parceiro-feed";
+import ItemFeed from "./components/feed/item-feed";
+import AguardandoConfirmacao from "./components/aguardando-confirmacao";
 function App() {
+  const [musicas, setMusicas] = useState([]);
+  function listar() {
+    api
+      .get()
+      .then((respostaObtida) => {
+        console.log(respostaObtida.data);
+        setMusicas(respostaObtida.data);
+      })
+      .catch((erroOcorrido) => {
+        console.log(erroOcorrido);
+      });
+  }
 
   return (
     <>
