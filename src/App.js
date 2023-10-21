7fbf6001ae97cf5640d12d7d423b95e7a17ba06a
FilipeRicardoSilva <filipe.silva@sptech.school>
2023-10-21 17:12:38 -0300

    move

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "./css/index.css";
 import "./css/responsivo.css";
-import BootstrapCarousel from "./components/carrossel";
-import FeedParceiro from "./components/feed-parceiro";
 
 function App() {
 
   return (
     <>
-    <Index/>
-    <CadastroInicial/>
-    <AguardandoConformacao/>
-    <ItemFeed/>
-    <FeedParceiro/>
+
     </>
   );
 }
