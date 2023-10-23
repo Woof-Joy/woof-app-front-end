53ef33536eab24e4e472901940d75bfbbc0e20ac
FilipeRicardoSilva <filipe.silva@sptech.school>
2023-10-23 20:22:13 -0300

    update logOff

--- src/components/MenuCliente.jsx
+++ src/components/MenuCliente.jsx
@@ -7,63 +7,67 @@ import { Link } from 'react-router-dom';
 
 
 function Menu() {
+    const cleanSessionStorage = () => {
+        sessionStorage.clear()
+
+    }
     return (
         <>
-                <header>
-                    <nav class="cabecalho-menu">
-                        <ul class="menu-navbar">
+            <header>
+                <nav class="cabecalho-menu">
+                    <ul class="menu-navbar">
 
-                            <div class="btn-lateral-menu">
-                                <i class="bi bi-list"></i>
-                                <img class="logo-img-navbar" src={ImgLogoWoofJoy} alt="logo branca da Woof Joy"></img>
-                            </div>
-                            <div class="space-menor"></div>
-                            <div class="todos-menu-item">
-                                <li class="menu-item">
-                                    <a href="#">
-                                        <span class="icon"><i class="bi bi-house-heart-fill"></i></span>
-                                        <span class="txt-link">Feed</span>
-                                    </a>
-                                </li>
-                                <li class="menu-item">
-                                    <a href="#">
-                                        <span class="icon"><i class="bi bi-chat-dots-fill"></i></span>
-                                        <span class="txt-link">Chat</span>
-                                    </a>
-                                </li>
-                                <li class="menu-item">
-                                    <a href="#">
-                                        <span class="icon"><i class="bi bi-bag-heart-fill"></i></span>
-                                        <span class="txt-link">Doação</span>
-                                    </a>
-                                </li>
-                                <li class="menu-item">
-                                    <a href="#">
-                                        <span class="icon"><i class="bi bi-clock-history"></i></span>
-                                        <span class="txt-link">Histórico</span>
-                                    </a>
-                                </li>
-                            </div>
-                            <div class="space"></div>
-                            <div class="menu-item-meu-perfil">
+                        <div class="btn-lateral-menu">
+                            <i class="bi bi-list"></i>
+                            <img class="logo-img-navbar" src={ImgLogoWoofJoy} alt="logo branca da Woof Joy"></img>
+                        </div>
+                        <div class="space-menor"></div>
+                        <div class="todos-menu-item">
+                            <li class="menu-item">
                                 <a href="#">
-                                    <span class="icon"><i class="bi bi-person-circle"></i></span>
-                                    <span class="txt-link">Meu Perfil</span>
+                                    <span class="icon"><i class="bi bi-house-heart-fill"></i></span>
+                                    <span class="txt-link">Feed</span>
                                 </a>
-                            </div>
-                            <div class="space-menor"></div>
-                            <hr></hr>
-                                <div class="space"></div>
-                                <Link to= "/" class="menu-item-sair">
-                                    <a href="#">
-                                        <span class="icon"><i class="bi bi-box-arrow-left"></i></span>
-                                        <span class="txt-link">Sair</span>
-                                    </a>
-                                </Link>
-                                <div class="space"></div>
-                        </ul>
-                    </nav>
-                </header>
+                            </li>
+                            <li class="menu-item">
+                                <a href="#">
+                                    <span class="icon"><i class="bi bi-chat-dots-fill"></i></span>
+                                    <span class="txt-link">Chat</span>
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="#">
+                                    <span class="icon"><i class="bi bi-bag-heart-fill"></i></span>
+                                    <span class="txt-link">Doação</span>
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="#">
+                                    <span class="icon"><i class="bi bi-clock-history"></i></span>
+                                    <span class="txt-link">Histórico</span>
+                                </a>
+                            </li>
+                        </div>
+                        <div class="space"></div>
+                        <div class="menu-item-meu-perfil">
+                            <a href="#">
+                                <span class="icon"><i class="bi bi-person-circle"></i></span>
+                                <span class="txt-link">Meu Perfil</span>
+                            </a>
+                        </div>
+                        <div class="space-menor"></div>
+                        <hr></hr>
+                        <div class="space"></div>
+                        <Link to="/" class="menu-item-sair" onClick={cleanSessionStorage}>
+                            <a href="#">
+                                <span class="icon"><i class="bi bi-box-arrow-left"></i></span>
+                                <span class="txt-link" >Sair</span>
+                            </a>
+                        </Link>
+                        <div class="space"></div>
+                    </ul>
+                </nav>
+            </header>
         </>
     );
 }
