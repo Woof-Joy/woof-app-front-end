439f3ffb8c5cc3c72603b947eb4578e52fcfae90
bianca-tsuchiya <bianca.tsuchiya@sptech.school>
2023-11-09 21:00:19 -0300

    adicionando inicio chat

---
+++ src/components/chat.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import MenuCliente from "../components/MenuCliente"
+
+function Chat() {
+    return (
+        <>
+            <MenuCliente className="menu" />
+
+        </>
+    )
+}
