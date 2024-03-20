973d502c8a43367c5df4b3fa8eced9225ab81751
FilipeRicardoSilva <filipe.silva@sptech.school>
2024-03-20 16:17:30 -0300

    update endpoint

--- src/woof-joy-api.js
+++ src/woof-joy-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const woofJoyApi = axios.create({
-    baseURL:"http://10.0.0.162:8080"
+    baseURL:"http://10.0.0.181:8080"
 
     //baseURL: "http://localhost:8080",
   });
