99ec1a93aa62305cf0fd9e2fe369a5cb9852d844
Pedro Oliveira Lopes <110927673+pedrooliveiralopes@users.noreply.github.com>
2024-03-21 01:33:38 -0300

    Update woof-joy-api.js

--- src/woof-joy-api.js
+++ src/woof-joy-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const woofJoyApi = axios.create({
-    baseURL:"http://50.17.103.74:8080"
+    baseURL:"http://ip-10-0-0-23.ec2.internal:8080"
 
     //baseURL: "http://localhost:8080",
   });
