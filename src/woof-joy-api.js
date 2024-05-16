398ce1a0ea8e09f8a080ae154802f189026da765
FilipeRicardoSilva <filipe.silva@sptech.school>
2024-05-16 12:34:48 -0300

    update host by docker balance nginx

--- src/woof-joy-api.js
+++ src/woof-joy-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const woofJoyApi = axios.create({
-    baseURL: "/backend",
-  });
+  baseURL: "http://172.17.0.4:3002", // Endereço IP interno do container NGINX
+});
 export default woofJoyApi;
