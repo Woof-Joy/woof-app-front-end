7caa032bd5ee2ed8c1679080fabffe52a1695997
FilipeRicardoSilva <filipe.silva@sptech.school>
2024-05-16 13:15:51 -0300

    teste local ip

--- src/woof-joy-api.js
+++ src/woof-joy-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const woofJoyApi = axios.create({
-  baseURL: "http://172.17.0.4:3002", // Endereço IP interno do container NGINX
+  baseURL: "http://localhost:80", 
 });
 export default woofJoyApi;
