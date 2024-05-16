ff58def1f15a2001a622fdcef96a471b88853e61
FilipeRicardoSilva <filipe.silva@sptech.school>
2024-05-16 20:17:48 -0300

    connect host

--- src/woof-joy-api.js
+++ src/woof-joy-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const woofJoyApi = axios.create({
-    baseURL: "http://10.0.0.25:8080/api",
+    baseURL: "http://localhost:8080/api",
   });
 export default woofJoyApi;
